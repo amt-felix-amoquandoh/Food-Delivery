d2316daef1887748865e43faf46065701038a2ad
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2022-12-13 23:31:01 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,5 +3,11 @@ const navbar = document.querySelector(".navBar");
 const menuCollapse = document.querySelector(".menuCollapse");
 
 menuToggle.addEventListener("click", () => {
-  navbar.classList.toggle("");
+  navbar.classList.toggle("showMenu");
+  if (navbar.classList.contains("showMenu")) {
+    menuCollapse.style.maxHeight = menuCollapse.scrollHeight + "px";
+  } else {
+    menuCollapse.style.maxHeight = "0px";
+    menuCollapse.style.overflow = "hidden";
+  }
 });
